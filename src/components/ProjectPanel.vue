c49a0002d2f14e83bce00fe40d4586fb23bc29ac
Jack <ajackofsometrades@gmail.com>
2018-11-08 09:51:47 -0800

    moving where I store the data, plugging in the filter/sort

--- src/components/ProjectPanel.vue
+++ src/components/ProjectPanel.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="search-bar">
-    <ProjectSearch/>
+  <div class="project-panel">
+    <ProjectSearch
+      :onFilter="handleFilter"
+      :onSort="handleSort"
+    />
     <ul>
       <ProjectCard
-        v-for="project in projects"
+        v-for="project in sortedProjects"
         :key="project.title"
         :project="project"
         :selected="selected"
@@ -19,16 +22,87 @@
 import ProjectCard from './ProjectCard';
 import ProjectSearch from './ProjectSearch';
 
+
+
+
 export default {
 
   components: {
     ProjectCard, ProjectSearch
   },
   props: {
-    projects: Array,
     selected: Object,
     onSelect: Function
-  }
+  },
+  data() {
+    return {
+      filter: {
+        query: ''
+      },
+      sort: {
+        sort: 'date',
+        direction: 1
+      },
+      projects: [
+        {
+          date: new Date(2018, 10, 24),
+          title: 'Banana Republic',
+          summary: 'The banana empire ipsum, solely devoted to providing me with bananas',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sed odio eos odit enim voluptates doloribus, corrupti similique repellat illum nisi, ratione eius magni natus fuga, laudantium minima esse! Provident.',
+          tags: ['Node', 'React', 'Express', 'Mongoose', 'MongoDB']
+        },
+        {
+          date: new Date(2018, 10, 21),
+          title: 'Chair',
+          summary: 'I love chair. Do you really love the chair? Yes. I love chair.',
+          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio amet vel, commodi laborum, molestiae, consequatur similique totam quod neque adipisci voluptatem porro consequuntur aspernatur assumenda facilis nulla voluptates obcaecati eveniet.',
+          tags: ['Node', 'Vue', 'Express', 'PostgresSQL']
+        },
+        {
+          date: new Date(2018, 9, 30),
+          title: 'Raiders',
+          summary: 'Why are the Raiders so bad? :(',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur doloribus molestiae perferendis atque, optio quam numquam dignissimos debitis harum id non aperiam veritatis dicta reiciendis tempora eos quia et enim?',
+          tags: ['Node', 'Vue', 'MongoDB', 'Mongoose']
+        }
+      ],
+    };
+  },
+  methods: {
+    handleFilter(filter) {
+      this.filter = filter;
+    },
+    handleSort(sort) {
+      this.sort = sort;
+    }
+  },
+  computed: {
+    filteredProjects() {
+      const { query } = this.filter;
+      if(!query) return this.projects;
+
+      return this.projects.filter(project => {
+        return (project.tags.some(tag => tag === query));
+      });
+    },
+    sortedProjects() {
+      const { sort, direction } = this.sort;
+      // if(!sort) {
+      //   sort = 'date';
+      // }
+
+      return this.filteredProjects
+        .slice()
+        .sort((a, b) => {
+          if(a[sort] > b[sort]) return 1 * direction;
+          if(a[sort] < b[sort]) return -1 * direction;
+          if(a[sort] === b[sort]) return 0;
+        });
+
+    }
+  },
+
+  
 
 };
 
@@ -36,7 +110,7 @@ export default {
 
 <style scoped lang="postcss">
 
-.search-bar {
+.project-panel {
   display: grid;
   grid-template-rows: 10% auto;
 
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,7 +2,6 @@
 <template>
   <div class="projects">
       <ProjectPanel
-        :projects="projects" 
         :selected="selectedProject"
         :onSelect="handleSelect"
       />
@@ -23,14 +22,7 @@ export default {
   },
   data() {
     return {
-      filter: {
-        query: ''
-      },
-      sort: {
-        sort: 'date',
-        direction: 1
-      },
-      projects: [
+      selectedProject:
         {
           date: new Date(2018, 10, 24),
           title: 'Banana Republic',
@@ -38,51 +30,7 @@ export default {
           description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sed odio eos odit enim voluptates doloribus, corrupti similique repellat illum nisi, ratione eius magni natus fuga, laudantium minima esse! Provident.',
           tags: ['Node', 'React', 'Express', 'Mongoose', 'MongoDB']
         },
-        {
-          date: new Date(2018, 10, 21),
-          title: 'Chair',
-          summary: 'I love chair. Do you really love the chair? Yes. I love chair.',
-          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio amet vel, commodi laborum, molestiae, consequatur similique totam quod neque adipisci voluptatem porro consequuntur aspernatur assumenda facilis nulla voluptates obcaecati eveniet.',
-          tags: ['Node', 'Vue', 'Express', 'PostgresSQL']
-        },
-        {
-          date: new Date(2018, 9, 30),
-          title: 'Raiders',
-          summary: 'Why are the Raiders so bad? :(',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur doloribus molestiae perferendis atque, optio quam numquam dignissimos debitis harum id non aperiam veritatis dicta reiciendis tempora eos quia et enim?',
-          tags: ['Node', 'Vue', 'MongoDB', 'Mongoose']
-        }
-      ],
-      selectedProject: null
     };
-  },
-  mounted() {
-    this.selectedProject = this.projects[0];
-  },
-  computed: {
-    filteredProjects() {
-      const { query } = this.filter;
-      if(!query) return this.projects;
-
-      return this.projects.filter(project => {
-        return (project.tags.some(tag => tag === query));
-      });
-    },
-    sortedProjects() {
-      let { sort, direction } = this.sort;
-      if(!sort) {
-        sort = 'date';
-      }
-
-      return this.sortedProjects
-        .slice()
-        .sort((a, b) => {
-          if(a[sort] > b[sort]) return 1 * direction;
-          if(a[sort] < b[sort]) return -1 * direction;
-          if(a[sort] === b[sort]) return 0;
-        });
-
-    }
   },
   methods: {
     handleSelect(project) {
