4ceef8356d329a72007314dce5ccf44c7dac687e
Jack <ajackofsometrades@gmail.com>
2018-11-09 08:20:50 -0800

    styling the project view more

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -41,6 +41,7 @@ export default {
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   font-size: 18px;
 
+
   &:hover {
     background-color: var(--lightgreen);
   }
--- src/components/ProjectView.vue
+++ src/components/ProjectView.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="proj-view">
-    <h2>{{ project.title }}</h2>
-    <p class="description">{{ project.description }}</p>
+  <div class="project-view">
+    <div class="project-content" v-if="project">
+      <h2>{{ project.title }}</h2>
+      <p class="description">{{ project.description }}</p>
+      
+      <h3>Links</h3>
+      <ul>
+        <li>
+          <a :href="project.github" target="_blank">Github</a>
+        </li>
+        <li>
+          <a :href="project.live" target="_blank">Live site</a>
+        </li>
+      </ul>
 
-    <br>
+      <br>
 
-    <div
-      v-for="tag in project.tags"
-      :key="tag"
-      class="tag-box"
-    >
-      <TechIcon :tag="tag"/>
-      <p class="tag">{{ tag }}</p>
+      <div
+        v-for="tag in project.tags"
+        :key="tag"
+        class="tag-box"
+      >
+        <TechIcon :tag="tag"/>
+        <p class="tag">{{ tag }}</p>
+      </div>
+    </div>
+    <div class="call-to-action" v-else>
+      <h2>
+        Select a project on the left!
+      </h2>
     </div>
   </div>
 </template>
@@ -34,29 +51,47 @@ export default {
 
 <style scoped lang="postcss">
 
-.proj-view {
+.project-view {
   border-top: 1px solid black;
   border-bottom: 1px solid black;
+
+  .project-content {
+
+    .description {
+      margin: 10px 100px;
+      text-align: left;
+    }
+
+    .tag-box {
+      display: inline-block;
+      margin: 10px;
+
+      .tag {
+        display: inline;
+      }
+    }
+
+    ul {
+      margin: 0 auto;
+      list-style: none;
+      padding-inline-start: 0;
+
+      li {
+        a {
+          text-decoration: none;
+          color: var(--darkgreen);
+        }
+      }
+    }
+
+    h3 {
+      margin: 10px auto;
+    }
+  }
 }
 
 h2 {
   margin: 40px;
 }
-
-.tag-box {
-  display: inline-block;
-  margin: 10px;
-}
-
-.description {
-  margin: 10px 100px;
-  text-align: left;
-}
-
-.tag {
-  display: inline;
-}
-
-
 
 </style>
